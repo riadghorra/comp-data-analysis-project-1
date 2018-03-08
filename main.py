3a9160911696989042594e4308204800cd7b501a
Wisse Barkhof <wisse@wisses-mbp.eduroam.wireless.dtu.dk>
2018-03-08 13:55:04 +0100

    merge

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ Created on Thu Mar  8 13:35:03 2018
 @author: wisse
 """
 
+
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from IPython.display import display, HTML
+from preprocessing import oneOutOfK, missing_predictor_as_mean, missing_predictor_as_value, missing_predictor_as_knn
 
 # load and display data
 data = pd.read_csv('Case1_Data.csv')
 pd.options.display.max_rows = 100
 pd.options.display.max_columns = 100
-
-pd.head()
 
 # split data in training / prediction based of Y value
 training = data.loc[data['Y'].notnull()]
@@ -26,3 +28,14 @@ print ('We have', len(training), 'training observations and',
 
 train_y = training['Y'].as_matrix()
 train_X = training[[col for col in training.columns if col != 'Y']].as_matrix()
+
+# One out of K encoding for categorical value
+train_X = oneOutOfK(train_X, 99)
+
+# replace missing values with mean of column
+train_X_mn = missing_predictor_as_mean(train_X)
+
+# replace missing values with mean of KNN
+clean_data = missing_predictor_as_value(train_X, 0)
+train_X_knn = missing_predictor_as_knn(5, train_X, clean_data)
+
